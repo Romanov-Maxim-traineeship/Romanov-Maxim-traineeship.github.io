f4dbacbd9af717f6895df17c6c4e188458e3e851
Romanov-Maxim-traineeship <42423787+Romanov-Maxim-traineeship@users.noreply.github.com>
2018-09-14 21:42:22 +0300

    pravka

--- pass/gen_pass.rb
+++ pass/gen_pass.rb
@@ -1,51 +1,51 @@
 class Generate 
-    attr_accessor :width, :number, :number_size
-    def initialize (width = 6, number = 0, number_size = 3)
-      @width = width
-      @number = number
-      @number_size = number_size
+  attr_accessor :width, :number, :number_size
+  def initialize (width = 6, number = 0, number_size = 3)
+    @width = width
+    @number = number
+    @number_size = number_size
+  end
+  def random_vowels()
+    vowels = ["a","e","i","y","o","u"]
+    vowels[rand(vowels.size)]
+  end
+  def random_consonants()
+    consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","w","z"]
+    consonants[rand(consonants.size)]
+  end
+  def pass()
+    case @number
+    when 0
+    i = 0
+    remain = 5-(@width%5)
+    @width += remain
+    word = ""
+    while i < @width
+      i +=2
+      word += random_consonants() + random_vowels()
     end
-    def random_vowels()
-      vowels = ["a","e","i","y","o","u"]
-      vowels[rand(vowels.size)]
+      @width -= remain
+    when 1
+    i = 0
+    j = 0
+    unit = ""
+    @width -= @number_size
+    while j < @number_size
+      j += 1
+      unit += (Random.rand(10)).to_s
     end
-    def random_consonants()
-      consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","w","z"]
-      consonants[rand(consonants.size)]
-    end
-    def pass()
-      case @number
-      when 0
-      i = 0
-      remain = 5-(@width%5)
-      @width += remain
-      word = ""
+    remain = 5-(@width%5)
+    @width += remain
+    word = ""
       while i < @width
         i +=2
         word += random_consonants() + random_vowels()
       end
-        @width -= remain
-      when 1
-      i = 0
-      j = 0
-      unit = ""
-      @width -= @number_size
-      while j < @number_size
-        j += 1
-        unit += (Random.rand(10)).to_s
-      end
-      remain = 5-(@width%5)
-      @width += remain
-      word = ""
-        while i < @width
-          i +=2
-          word += random_consonants() + random_vowels()
-        end
-          @width -= remain
-      end
-      result = word[0,@width]
-      result += unit
+    @width -= remain
     end
+    result = word[0,@width]
+    result += unit
+  end
 
 end
 
