d5c6f71c5c84b0896e0005be3699837e4b48dac4
Romanov-Maxim-traineeship <42423787+Romanov-Maxim-traineeship@users.noreply.github.com>
2018-09-16 13:13:47 +0300

    pravka style guide

--- pass/gen_pass.rb
+++ pass/gen_pass.rb
@@ -1,60 +1,55 @@
 class Generate 
   attr_accessor :width, :number, :number_size
-
   def initialize (width = 6, number = 0, number_size = 3)
     @width = width
     @number = number
     @number_size = number_size
+    @VOWELS = ["a","e","i","y","o","u"]
+    @CONSONANTS = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","w","z"]
   end
 
-  def random_vowels()
-    vowels = ["a","e","i","y","o","u"]
-    vowels[rand(vowels.size)]
+  def random_syllable
+    @VOWELS[rand(@VOWELS.size)] + @CONSONANTS[rand(@CONSONANTS.size)]
   end
 
-  def random_consonants()
-    consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","w","z"]
-    consonants[rand(consonants.size)]
-  end
-
-  def pass()
-    case @number
-    when 0
-    i = 0
-    remain = 5-(@width%5)
-    @width += remain
-    word = ""
-    while i < @width
-      i +=2
-      word += random_consonants() + random_vowels()
-    end
-      @width -= remain
-      result = word[0,@width]
-    when 1
-    i = 0
+  def random_number
     j = 0
     unit = ""
-    @width -= @number_size
     while j < @number_size
       j += 1
       unit += (Random.rand(10)).to_s
     end
-    remain = 5-(@width%5)
-    @width += remain
-    word = ""
+    return  unit
+  end
+
+  def pass
+    i = 0
+    case @number
+    when 0
+      remain = 5-(@width%5)
+      @width += remain
+      word = ""
       while i < @width
         i +=2
-        word += random_consonants() + random_vowels()
+        word += random_syllable
       end
-    @width -= remain
-    result = word[0,@width]
-    result += unit
+        @width -= remain
+        result = word[0,@width]
+    when 1
+      @width -= @number_size
+      
+      remain = 5-(@width%5)
+      @width += remain
+      word = ""
+        while i < @width
+          i +=2
+          word += random_syllable
+        end
+      result = word[0,(@width - remain)] + random_number
     end
-
   end
-  
 end
 
-item = Generate.new(8)
+item = Generate.new(8, 1, 3)
 puts "Ваш пароль: #{item.pass()}"
 
