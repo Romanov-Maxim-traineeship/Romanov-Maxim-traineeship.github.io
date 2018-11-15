f5273f5bc17f1c30177049d74ff405cf35472c78
Romanov-Maxim-traineeship <42423787+Romanov-Maxim-traineeship@users.noreply.github.com>
2018-11-16 02:13:46 +0300

    translatorv0.01

---
+++ Aleksey_Yandex_Translator/script.js
@@ -0,0 +1,53 @@
+ var yandex_translator = {
+      lines : {},
+      key : 'trnsl.1.1.20181113T124855Z.265a3dbb4ed09a6b.898310b6c4a33ca9d35f5d9c39561a578b93a8b3',
+      api : 'https://translate.yandex.net/api/v1.5/tr.json/translate',
+
+      translate_text : function (id, callback) {
+
+        var text = document.getElementById(id).innerHTML;
+        var url = this.api + '?';
+        url += 'key=' + this.key + '&text=' + text + '&lang=ru-en';
+
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', url, true);
+        ajax.onreadystatechange = function() {
+          if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+              text = JSON.parse(ajax.responseText);
+              callback(text.text[0]);
+            }
+          }
+        }
+        ajax.send(null);
+
+
+
+
+        //-----------
+          /*
+                  var text_call = new XMLHttpRequest();
+
+        text_call.open('GET', url, false);
+
+        text_call.send();
+
+        if (text_call.status == 200) {
+          alert(text_call.responseText);
+        }
+
+        */
+        //-----------
+      },
+
+      revert : function (id) {
+
+      }
+
+
+      
+    }
+
+    log = function (text){
+        console.log(text);
+      }
